79d283aa0dd64b9b3a45dd494d70432a37b5daaa
Pedrotvgames <ph261644@gmail.com>
2026-04-17 10:05:35 -0300

    Alarme
    Alarme de saida para os livros não alugados

--- API/LeitorSaida/consumoAPI.py
+++ API/LeitorSaida/consumoAPI.py
@@ -1,37 +1,53 @@
 import requests
+import platform
+import pygame
+import time  # 👉 A biblioteca foi adicionada aqui!
+
+pygame.mixer.init()
 
 
+def disparar_alarme():
+    try:
+        # Carrega o arquivo de som
+        pygame.mixer.music.load("AlarmeDeSaida.mpeg")
+        pygame.mixer.music.play()
 
+        # Define quanto tempo o alarme vai tocar (em segundos)
+        tempo_de_toque = 2.0
+        time.sleep(tempo_de_toque)  # Espera esse tempo passar
 
+        # Para a música à força depois que o tempo passou
+        pygame.mixer.music.stop()
+
+    except Exception as e:
+        print("Erro ao tentar tocar o áudio:", e)
 
 def verificar_livro(rfid_do_livro):
-    # A URL onde o seu servidor Flask está rodando.
-    # Altere a porta (5000) se o seu servidor estiver usando outra.
-    url = 'http://192.168.80.253:5000/api/livros/consultar'
-
-    # Os dados que vamos enviar no formato JSON
-    dados_enviados = {
-        "rfid": rfid_do_livro
-    }
+    url = 'http://192.168.82.73:8000/livros/consultar'
+    dados_enviados = {"rfid": rfid_do_livro}
 
     try:
         # Fazendo a requisição POST para a API
         resposta = requests.post(url, json=dados_enviados)
 
-        # Verifica se o código de status HTTP é 200 (OK)
+        # 1. LIVRO ENCONTRADO NO BANCO (Mas precisamos ver se está alugado)
         if resposta.status_code == 200:
             dados_retornados = resposta.json()
             print(f"Livro: {dados_retornados['nomeLivro']}")
             print(f"Status: {dados_retornados['mensagem']}")
 
-        # Verifica se o código é 404 (Não encontrado)
+            # Se "disponivel" for True, significa que NÃO FOI ALUGADO!
+            if dados_retornados.get('disponivel') == True:
+                print("🚨 ALARME! TENTATIVA DE SAÍDA COM LIVRO NÃO ALUGADO!")
+                disparar_alarme() # 👉 Toca o som!
+
+        # 2. LIVRO NÃO CADASTRADO (Pode ser um livro do acervo sem registro ou etiqueta errada)
         elif resposta.status_code == 404:
-            print("Erro: Livro não cadastrado no banco de dados.")
+            print("🚨 ALARME! ETIQUETA NÃO RECONHECIDA/NÃO CADASTRADA!")
+            disparar_alarme() # 👉 Toca o som!
 
         else:
             print(f"Erro inesperado: Status {resposta.status_code}")
 
     except requests.exceptions.ConnectionError:
         print("Erro: Não foi possível conectar ao servidor. O Flask está rodando?")
-
-
